28bef3afdd5c2e633aad2a0e85d4e1a3d0ee0196
Emma <emmadavis818@gmail.com>
2025-08-07 16:03:21 -0500

    editing test import & plot

--- GALINDA/tests/test_import.py
+++ GALINDA/tests/test_import.py
@@ -9,9 +9,15 @@ rc("animation", html = "html5")
 from GALINDA import GALINDA
 
 # %%
-gal = GALINDA.Bubble("animation_data/", "m11h_star_coordinates")
-# %%
-hasattr(gal,"path")
-hasattr(gal,"fnames")
-hasattr(gal,"key")
-hasattr(gal,"ind")
+def test_import():
+    """
+    This function tests that we can import data and have assigned attributes of path, fnames, key, and ind.
+    """
+    gal = GALINDA.Bubble("animation_data/", "m11h_star_coordinates")
+    assert hasattr(gal,"path") == True
+    assert hasattr(gal,"fnames") == True
+    assert hasattr(gal,"key") == True
+    assert hasattr(gal,"ind") == True
+
+if __name__ == "__main__":
+    test_import()
--- GALINDA/tests/test_plot.py
+++ GALINDA/tests/test_plot.py
@@ -9,7 +9,20 @@ rc("animation", html = "html5")
 from GALINDA import GALINDA
 
 # %%
-gal = GALINDA.Bubble("animation_data/", "m11h_star_coordinates")
-plot = gal.plot(0)
-hasattr(gal,"fig")
-hasattr(gal,"im")
+def test_plot():
+    """
+    Test that plot function produces a plot of the screenshot file for animation.
+    This function will also assert that the given attributes fig and im were assigned.
+
+    We'll test on the 0th file.
+    """
+    gal = GALINDA.Bubble("animation_data/", "m11h_star_coordinates")
+    plot = gal.plot(0)
+    
+    assert hasattr(gal,"fig") == True
+    assert hasattr(gal,"im") == True
+
+    return plot
+
+if __name__ == "__main__":
+    test_plot()
